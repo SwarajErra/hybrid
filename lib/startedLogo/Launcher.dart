215fcb9d90668135112378d637b1692fad42c2c3
tejveer2608 <hayertejveersingh@gmail.com>
2021-12-03 16:40:41 -0500

    adding Launcher.dart

---
+++ lib/startedLogo/Launcher.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class launcher extends StatefulWidget {
+  const launcher({Key? key}) : super(key: key);
+
+  @override
+  _launcherState createState() => _launcherState();
+}
+
+class _launcherState extends State<launcher> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      height: 400,
+      width: 400,
+      color: Colors.red,
+      child: Column(
+
+      ),
+    );
+  }
+}
