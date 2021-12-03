05119e5d353e6385b66ad5ae6947a4b3d8abfaa5
tejveer2608 <hayertejveersingh@gmail.com>
2021-12-03 18:10:13 -0500

    adding splash screen

--- lib/startedLogo/Launcher.dart
+++ lib/startedLogo/Launcher.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import './Optionpage.dart';
 
 class launcher extends StatefulWidget {
   const launcher({Key? key}) : super(key: key);
@@ -10,14 +12,27 @@ class launcher extends StatefulWidget {
 
 class _launcherState extends State<launcher> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+        Duration(seconds: 5),
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => MakeChoice())));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
+      margin: EdgeInsets.fromLTRB(10, 150, 10, 10),
       height: 400,
       width: 400,
-      color: Colors.red,
       child: Column(
-
+        children: [
+          Image.asset("Assest/images/logo.png", width: 200, height: 300),
+          Text("Getwork",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+        ],
       ),
     );
   }
---
+++ lib/startedLogo/Optionpage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class MakeChoice extends StatefulWidget {
+
+
+  @override
+  _MakeChoiceState createState() => _MakeChoiceState();
+}
+
+class _MakeChoiceState extends State<MakeChoice> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
