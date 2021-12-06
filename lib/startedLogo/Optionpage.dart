a384dcdb1d44e5956f37cd8be0bef4dbb7c378f4
tejveer2608 <hayertejveersingh@gmail.com>
2021-12-06 15:34:13 -0500

    adding 2nd page

--- lib/startedLogo/Optionpage.dart
+++ lib/startedLogo/Optionpage.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
 
-
 class MakeChoice extends StatefulWidget {
-
-
   @override
   _MakeChoiceState createState() => _MakeChoiceState();
 }
 
 class _MakeChoiceState extends State<MakeChoice> {
+  int _volume = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
+    final ButtonStyle style1 = ElevatedButton.styleFrom(
+        textStyle: const TextStyle(fontSize: 36, color: Colors.yellow));
+    return Container(
+      color: Colors.red,
+      child: Expanded(
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: null,
+                    style: style1,
+                    child: Text("Job seeker"),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: null,
+                    style: style1,
+                    child: Text("Employeer"),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
