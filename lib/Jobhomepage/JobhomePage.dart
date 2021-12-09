014eb292f2b03c0fabf6630d15bb447d3e64f9f9
tejveer2608 <hayertejveersingh@gmail.com>
2021-12-09 11:28:17 -0500

    adding search jobs

--- lib/Jobhomepage/JobhomePage.dart
+++ lib/Jobhomepage/JobhomePage.dart
@@ -10,26 +10,43 @@ class jobhomePage extends StatefulWidget {
 }
 
 class _jobhomePageState extends State<jobhomePage> {
+  String titleInput = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Image.asset(
-          'Assest/images/logo.png',
+        leading:
+        Image.asset(
+          'Assest/images/logoNew.png',
         ),
         actionsIconTheme: IconThemeData(color: Colors.yellow, size: 36),
         backgroundColor: Color(0xff5ECC1A),
+        elevation: (5),
+        titleSpacing: 7,
         title: Text(
           "Getjob",
-          style: GoogleFonts.roboto(fontSize: 25),
+          style: GoogleFonts.roboto(fontSize: 25,color: Colors.white),
         ),
-
       ),
       endDrawer: navigationDrawer(),
-      body: Container(
+      body: Card(
+        child: Container(
+          padding: EdgeInsets.all(5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                onChanged: (value) {
+                  titleInput = value;
+                },
+                decoration: InputDecoration(labelText: "Search jobs"),
+                style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
-
