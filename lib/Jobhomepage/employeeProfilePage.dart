33b1e9234d7f645a2db71e5acbd9277552386952
tejveer2608 <82590817+tejveer1031@users.noreply.github.com>
2021-12-17 16:03:01 -0500

    added user profile page

--- lib/Jobhomepage/employeeProfilePage.dart
+++ lib/Jobhomepage/employeeProfilePage.dart
@@ -1,26 +1,23 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class EmployeeProfile extends StatefulWidget {
-  const EmployeeProfile({Key? key}) : super(key: key);
+void main() => runApp(MaterialApp(
+  home:EmployeeProfilePage(),
+));
 
-  @override
-  _EmployeeProfileState createState() => _EmployeeProfileState();
-}
-
-class _EmployeeProfileState extends State<EmployeeProfile> {
+class EmployeeProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [
+        children: <Widget>[
           Container(
               decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [Colors.green, Colors.lightGreen]),
-                color: Colors.green,
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.redAccent, Colors.pinkAccent]
+                  )
               ),
               child: Container(
                 width: double.infinity,
@@ -29,18 +26,16 @@ class _EmployeeProfileState extends State<EmployeeProfile> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
+                    children: <Widget>[
                       CircleAvatar(
-                        backgroundImage: AssetImage(
-                          "Assest/images/avatar.png",
-                        ),
-                        radius: 50.0,
+                        backgroundImage: AssetImage("Assest/1598083178379.png"),
+                        radius: 80.0,
                       ),
                       SizedBox(
                         height: 10.0,
                       ),
                       Text(
-                        "Tejveer",
+                        "Tejveer singh",
                         style: TextStyle(
                           fontSize: 22.0,
                           color: Colors.white,
@@ -50,23 +45,70 @@ class _EmployeeProfileState extends State<EmployeeProfile> {
                         height: 10.0,
                       ),
                       Card(
-                        margin: EdgeInsets.symmetric(
-                            horizontal: 20.0, vertical: 5.0),
+                        margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 5.0),
                         clipBehavior: Clip.antiAlias,
                         color: Colors.white,
-                        elevation: 5.0,
+                        elevation: 10,
                         child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 8.0, vertical: 22.0),
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 22.0),
                           child: Row(
-                            children: [
+                            children: <Widget>[
                               Expanded(
                                 child: Column(
-                                  children: [
+                                  children: <Widget>[
                                     Text(
-                                      "Jobs applied",
+                                      "Job applied",
                                       style: TextStyle(
+                                        color: Colors.redAccent,
+                                        fontSize: 22.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Text(
+                                      "3",
+                                      style: TextStyle(
+                                        fontSize: 20.0,
+                                        color: Colors.pinkAccent,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  children: <Widget>[
+                                    Text(
+                                      "Followers",
+                                      style: TextStyle(
+                                        color: Colors.redAccent,
+                                        fontSize: 22.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Text(
+                                      "555",
+                                      style: TextStyle(
+                                        fontSize: 20.0,
+                                        color: Colors.pinkAccent,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
 
+                                  children: <Widget>[
+                                    Text(
+                                      "Follow",
+                                      style: TextStyle(
+                                        color: Colors.redAccent,
                                         fontSize: 22.0,
                                         fontWeight: FontWeight.bold,
                                       ),
@@ -78,55 +120,7 @@ class _EmployeeProfileState extends State<EmployeeProfile> {
                                       "10",
                                       style: TextStyle(
                                         fontSize: 20.0,
-
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      "Followers",
-                                      style: TextStyle(
-
-                                        fontSize: 22.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 5.0,
-                                    ),
-                                    Text(
-                                      "210",
-                                      style: TextStyle(
-                                        fontSize: 20.0,
-
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      " Following",
-                                      style: TextStyle(
-
-                                        fontSize: 22.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 5.0,
-                                    ),
-                                    Text(
-                                      "300",
-                                      style: TextStyle(
-                                        fontSize: 20.0,
-
+                                        color: Colors.pinkAccent,
                                       ),
                                     )
                                   ],
@@ -139,43 +133,35 @@ class _EmployeeProfileState extends State<EmployeeProfile> {
                     ],
                   ),
                 ),
-              )),
+              )
+          ),
           Container(
             child: Padding(
-              padding:
-              const EdgeInsets.symmetric(vertical: 30.0, horizontal: 16.0),
+              padding: const EdgeInsets.symmetric(vertical: 12.0,horizontal: 16.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
+                children: <Widget>[
                   Text(
-                    "About:",
-                    style: TextStyle(
-                        color: Colors.green,
+                    "Bio:",
+                    style: GoogleFonts.roboto(textStyle: TextStyle(
+                        color: Colors.redAccent,
                         fontStyle: FontStyle.normal,
-                        fontSize: 28.0),
+                        fontSize: 24.0,fontWeight: FontWeight.bold
+                    ),
+                    ),
                   ),
                   SizedBox(
                     height: 10.0,
                   ),
-                  Row(children: [
-
-
-                  ],),
-                  Text(
-                    "Skills:",
-                    style: TextStyle(
-                        fontStyle: FontStyle.normal,fontWeight: FontWeight.bold,
-                        fontSize: 22.0),
-                  ),
-                  Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w300,
+                  Text('My name is Tejveer and I am  a freelance mobile app developper.\n'
+                      'if you need any mobile app for your company then contact me for more informations',
+                    style: GoogleFonts.openSans(textStyle:TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w700,
                       color: Colors.black,
                       letterSpacing: 2.0,
+                    ),
                     ),
                   ),
                 ],
@@ -187,10 +173,12 @@ class _EmployeeProfileState extends State<EmployeeProfile> {
           ),
           Container(
             width: 300.00,
+
             child: RaisedButton(
-                onPressed: () {},
+                onPressed: (){},
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(80.0)),
+                    borderRadius: BorderRadius.circular(80.0)
+                ),
                 elevation: 0.0,
                 padding: EdgeInsets.all(0.0),
                 child: Ink(
@@ -198,22 +186,19 @@ class _EmployeeProfileState extends State<EmployeeProfile> {
                     gradient: LinearGradient(
                         begin: Alignment.centerRight,
                         end: Alignment.centerLeft,
-                        colors: [Colors.green, Colors.lightGreen]),
+                        colors: [Colors.redAccent,Colors.pinkAccent]
+                    ),
                     borderRadius: BorderRadius.circular(30.0),
                   ),
                   child: Container(
-                    constraints:
-                    BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
+                    constraints: BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
                     alignment: Alignment.center,
-                    child: Text(
-                      "Call",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 26.0,
-                          fontWeight: FontWeight.w300),
+                    child: Text("Contact me",
+                      style: TextStyle(color: Colors.white, fontSize: 26.0, fontWeight:FontWeight.w300),
                     ),
                   ),
-                )),
+                )
+            ),
           ),
         ],
       ),
--- lib/drawer/Navigationdrawer.dart
+++ lib/drawer/Navigationdrawer.dart
@@ -1,5 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hybrid/Jobhomepage/employeeProfilePage.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 class navigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,7 +14,11 @@ class navigationDrawer extends StatelessWidget {
           createDrawerBodyItem(
               icon: Icons.home,text: 'Home'),
           createDrawerBodyItem(
-              icon: Icons.account_circle,text: 'Profile'),
+              icon: Icons.account_circle,text: 'Profile(Working)',onTap: (){
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => EmployeeProfilePage()));
+          }),
+
           createDrawerBodyItem(
               icon: Icons.event_note,text: 'Events'),
           Divider(),
@@ -20,8 +27,9 @@ class navigationDrawer extends StatelessWidget {
           createDrawerBodyItem(
               icon: Icons.contact_phone,text: 'Contact Info'),
           ListTile(
-            title: Text('click me'),
-            onTap: () {},
+            title: Text('Extra button'),
+            onTap: () {
+            },
           ),
         ],
       ),
@@ -36,8 +44,34 @@ Widget createDrawerBodyItem(
         Icon(icon),
         Padding(
           padding: EdgeInsets.only(left: 8.0),
-          child: Text(text),
-        )
+          child: GradientText(
+            text,
+              style: GoogleFonts.roboto(
+                textStyle: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              colors: [
+                Colors.red,
+                Colors.pink,
+                Colors.purple,
+                Colors.deepPurple,
+                Colors.deepPurple,
+                Colors.indigo,
+                Colors.blue,
+                Colors.lightBlue,
+                Colors.cyan,
+                Colors.teal,
+                Colors.green,
+                Colors.lightGreen,
+                Colors.lime,
+                Colors.yellow,
+                Colors.amber,
+                Colors.orange,
+                Colors.deepOrange,
+              ],
+        ),
+        ),
       ],
     ),
     onTap: onTap,
