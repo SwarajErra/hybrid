5c425a97eaa2fa52f6dfbbbd13582263946ac584
sravankumarrapolu143 <sravankumarrapolus@gmail.com>
2022-01-11 16:43:11 -0500

    Added  Home Search page with Integration with firebase

--- lib/Jobhomepage/Searchbar.dart
+++ lib/Jobhomepage/Searchbar.dart
@@ -41,9 +41,7 @@ class _searchBarState extends State<searchBar> {
 
   final List<addingAdd> addadds = employeerHomePage.addadds;
 
-
   Widget build(BuildContext context) {
-
     return Stack(
       children: <Widget>[
         Positioned.fill(
@@ -56,67 +54,6 @@ class _searchBarState extends State<searchBar> {
                   child: SingleChildScrollView(
                     child: Column(
                       children: <Widget>[
-                        Column(
-                          children: addadds.map((tx) {
-                            return Card(
-                              elevation: 15,
-                              child: Container(
-                                width: double.infinity,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Padding(padding: EdgeInsets.all(2)),
-                                    Text(
-                                      'Job Type:- ' + tx.jobType,
-                                      style: GoogleFonts.breeSerif(
-                                        textStyle: TextStyle(
-                                            fontSize: 20, wordSpacing: 1),
-                                      ),
-                                    ),
-                                    Text(
-                                      'Job Descreption:- ' + tx.jobDescrebtion,
-                                      style: GoogleFonts.breeSerif(
-                                        textStyle: TextStyle(
-                                            fontSize: 20, wordSpacing: 1),
-                                      ),
-                                    ),
-                                    Text(
-                                      'Company Location:- ' +
-                                          tx.companyLocation,
-                                      style: GoogleFonts.breeSerif(
-                                        textStyle: TextStyle(
-                                            fontSize: 20, wordSpacing: 1),
-                                      ),
-                                    ),
-                                    Text(
-                                      'Company Name:- ' + tx.companyName,
-                                      style: GoogleFonts.breeSerif(
-                                        textStyle: TextStyle(
-                                            fontSize: 20, wordSpacing: 1),
-                                      ),
-                                    ),
-                                    Text('\$${tx.payrate}'.toString(),
-                                        style: GoogleFonts.breeSerif(
-                                          textStyle: TextStyle(
-                                              fontSize: 20, wordSpacing: 1),
-                                        )),
-                                    Text(
-                                        DateFormat('MM-dd-yyy').format(tx.date),
-                                        style: GoogleFonts.breeSerif(
-                                          textStyle: TextStyle(
-                                              fontSize: 20, wordSpacing: 1),
-                                        )),
-                                  ],
-                                ),
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                        dynamicJobCard(),
-                        dynamicJobCard(),
-                        dynamicJobCard(),
-                        dynamicJobCard(),
-                        dynamicJobCard(),
                         dynamicJobCard(),
                       ],
                     ),
@@ -140,8 +77,17 @@ class dynamicJobCard extends StatefulWidget {
 }
 
 class _dynamicJobCardState extends State<dynamicJobCard> {
-
   Map<String, dynamic>? userMap;
+  var list = [];
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    onSearch();
+  }
+
 
   void onSearch() async {
     FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -149,68 +95,101 @@ class _dynamicJobCardState extends State<dynamicJobCard> {
       if (mounted) {
         setState(() {
           userMap = value.docs[0].data();
+          for (var i = 0; i < value.docs.length; i++) {
+            list.add(value.docs[i].data());
+          }
         });
       }
+      print(list);
       print(userMap);
     });
   }
+
+  dynamicCard(userMap){
+    return Card(
+      shadowColor: Colors.black,
+      elevation: 20,
+      child: Container(
+        height: 190,
+        width: 400,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+            Row(children: <Widget>[
+              Text("Job Type:",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+              Text(' ${userMap!["jobType"]}',
+                  style: GoogleFonts.openSans(
+                      textStyle: TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.normal)))
+            ]),
+
+            Row(children: <Widget>[
+              Text("Company name:",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+              Text(' ${userMap!["companyName"]}',
+                  style: GoogleFonts.openSans(
+                      textStyle: TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.normal)))
+            ]),
+            Row(children: <Widget>[
+              Text("Company location:",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+              Text(' ${userMap!["CompanyLocation"]}',
+                  style: GoogleFonts.openSans(
+                      textStyle: TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.normal)))
+            ]),
+            Row(children: <Widget>[
+              Text("Pay rate:",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+              Text(' ${userMap!["payRate"]}',
+                  style: GoogleFonts.openSans(
+                      textStyle: TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.normal)))
+            ]),
+
+            Row(children: <Widget>[
+              Text("Job description:",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+              Text(' ${userMap!["job Description"]}',
+                  style: GoogleFonts.openSans(
+                      textStyle: TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.normal)))
+            ]),
+
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                    label: Text('Apply',
+                        style: GoogleFonts.pacifico(
+                            textStyle: TextStyle(
+                              fontSize: 20,
+                            ))),
+                    icon: Icon(Icons.favorite),
+                    onPressed: () {
+                      print('Pressed');
+                      onSearch();
+                    },
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      child: Card(
-        shadowColor: Colors.black,
-        elevation: 20,
-        child: Container(
-          height: 190,
-          width: 400,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text('job type ----',
-                  style: GoogleFonts.openSans(
-                      textStyle: TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.normal))),
-              Text('Company name ----',
-                  style: GoogleFonts.openSans(
-                      textStyle: TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.normal))),
-              Text('Company location ----',
-                  style: GoogleFonts.openSans(
-                      textStyle: TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.normal))),
-              Text('Pay rate ---',
-                  style: GoogleFonts.openSans(
-                      textStyle: TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.normal))),
-              Text('job description ----',
-                  style: GoogleFonts.openSans(
-                      textStyle: TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.normal))),
-              Expanded(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton.icon(
-                      label: Text('Apply',
-                          style: GoogleFonts.pacifico(
-                              textStyle: TextStyle(
-                            fontSize: 20,
-                          ))),
-                      icon: Icon(Icons.favorite),
-                      onPressed: () {
-                        print('Pressed');
-                        onSearch();
-                      },
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      child:   Column(
+        children: [
+        for ( var i in list ) dynamicCard(i)
+    ],
+    )
     );
   }
 }
