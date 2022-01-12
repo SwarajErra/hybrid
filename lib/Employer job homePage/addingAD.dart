48daabff69e61f3291ad13a5ef7e7f88b192479e
tejveer2608 <82590817+tejveer1031@users.noreply.github.com>
2022-01-11 23:13:07 -0500

    Update addingAD.dart

--- lib/Employer job homePage/addingAD.dart
+++ lib/Employer job homePage/addingAD.dart
@@ -4,13 +4,12 @@ class addingAdd {
 final  String companyLocation ;
  final double payrate ;
  final String jobDescrebtion ;
- final DateTime date;
+
 
   addingAdd(
       { required this.jobType,
       required this.companyName,
       required this.jobDescrebtion,
       required this.companyLocation,
-      required this.date,
       required this.payrate});
 }
